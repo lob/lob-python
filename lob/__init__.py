230c2030064957fddb9c4025f480e8d127b94763
grpgrp <rparker@smbapps.com>
2014-01-03 20:54:40 -0500

    Also add files param to LobObject.make_request
    I missed this one on the previous commit.

--- lob/__init__.py
+++ lob/__init__.py
@@ -39,7 +39,7 @@ class LobRequestor(object):
     def _make_requests_request(self, method, url_suffix, data, files={}):
         full_url = '%s%s' % (self._api_base_url, url_suffix)
 
-        r = self._method_fn_map.get(method)(url=full_url, data=data, files=files
+        r = self._method_fn_map.get(method)(url=full_url, data=data, files=files,
                                             auth=(self._api_key, ''))
         content = json.loads(r.content)
         error = content.get('error', content.get('errors', []))  # if any error
@@ -141,10 +141,10 @@ class LobObject(object):
         return d
 
     @classmethod
-    def make_request(cls, method, url_suffix, data={}):
+    def make_request(cls, method, url_suffix, data={}, files={}):
         requestor = LobRequestor()
         resp = requestor.make_request(method=method, url_suffix=url_suffix,
-                                      data=data)
+                                      data=data, files={})
         return make_lob_object(response=resp)
 
 
