b40e010aec6641af82d7b65debe64e5aede4df18
Peter Nagel <pnagel5@gmail.com>
2014-07-15 09:55:19 -0700

    finished objects tets

--- tests/test_object.py
+++ tests/test_object.py
@@ -6,28 +6,47 @@ lob.api_key = 'test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc'
 class ObjectFunctions(unittest.TestCase):
     def setUp(self):
         lob.api_key = 'test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc'
+        self.obj = lob.Object.list(count=1).data[0]
 
     def test_list_objects(self):
-        print lob.Object.list()
+        objects = lob.Object.list()
+        self.assertTrue(isinstance(objects.data[0], lob.Object))
+        self.assertEqual(objects.object, 'list')
 
+    def test_list_objects_limit(self):
+        objects = lob.Object.list(count=2)
+        self.assertTrue(isinstance(objects.data[0], lob.Object))
+        self.assertEqual(len(objects.data), 2)
 
-    def test_list_objects_count_offset(self):
-        print lob.Object.list(count=1).data[0]
+    def test_list_objects_fail(self):
+        self.assertRaises(lob.error.InvalidRequestError, lob.Object.list, count=1000)
 
+    def test_create_object_remote(self):
+        object = lob.Object.create(
+            name = 'Test Object',
+            file = 'https://www.lob.com/test.pdf',
+            setting_id = 201
+        )
 
-    def test_find_object(self):
-        print lob.Object.retrieve(id=lob.Object.list(count=1).data[0].id)
+        self.assertTrue(isinstance(object, lob.Object))
+        self.assertEqual(object.name, 'Test Object')
 
+    def test_create_object_local(self):
+        object = lob.Object.create(
+            name = 'Test Object Inline',
+            file = open('tests/pc.pdf', 'rb'),
+            setting_id = 201
+        )
 
-    def test_delete_object(self):
-        print lob.Object.delete(id=lob.Object.list(count=1).data[0].id)
+        self.assertTrue(isinstance(object, lob.Object))
+        self.assertEqual(object.name, 'Test Object Inline')
 
+    def test_create_object_fail(self):
+        self.assertRaises(lob.error.InvalidRequestError, lob.Object.create)
 
-    def test_create_object(self):
-        print lob.Object.create(name='Test Object', file='https://www.lob.com/test.pdf',
-                setting_id=201, quantity=1)
+    def test_retrieve_job(self):
+        job = lob.Object.retrieve(id=lob.Object.list().data[0].id)
+        self.assertTrue(isinstance(job, lob.Object))
 
-        def test_create_object_with_local_file(self):
-            print lob.Object.create(name='Local File Object', file=open('tests/test.pdf','rb'),
-                    setting_id=100, quantity=1)
-
+    def test_retrieve_job_fail(self):
+        self.assertRaises(lob.error.InvalidRequestError, lob.Object.retrieve, id='test')
