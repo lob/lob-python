d236221e41ba7d82534e3b5069191f0124a63116
Peter Nagel <pnagel5@gmail.com>
2014-07-15 22:09:33 -0700

    upped version

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
+import sys
+import os
 try:
     from setuptools import setup
 except ImportError:
     from disutils.core import setup
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lob'))
-from version import VERSION
-
 setup (
         name = 'lob',
-        version = VERSION,
+        version = 2.0,
         author = 'Lob',
         author_email = 'support@lob.com',
         packages = ['lob'],
@@ -18,8 +17,4 @@ setup (
         install_requires = [
             'requests'
             ],
-        test_suite = 'nose.collector',
-        test_requires = [
-            'Nose'
-            ]
         )
