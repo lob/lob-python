1989326bc0c203bc8ce4b8ae72c288805e5b4b61
Peter Nagel <pnagel5@gmail.com>
2014-09-26 14:27:40 -0700

    added user agent header to requests

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup (
         name = 'lob',
-        version = '2.5',
+        version = '2.6',
         author = 'Lob',
         author_email = 'support@lob.com',
         packages = ['lob'],
