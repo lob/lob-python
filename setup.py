63adaeb7fda15a7c3f24a48b5d0d3bb307fa1cb1
Peter Nagel <pnagel5@gmail.com>
2014-11-17 19:17:09 -0500

    Bumped Version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup (
         name = 'lob',
-        version = '2.7',
+        version = '2.8',
         author = 'Lob',
         author_email = 'support@lob.com',
         packages = ['lob'],
