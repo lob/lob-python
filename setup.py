1f98f12afbe7dfeb5cebe0c74f1365e36f5df3e7
Peter Nagel <pnagel5@gmail.com>
2014-12-10 10:10:41 -0800

    Updated Version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup (
         name = 'lob',
-        version = '2.8',
+        version = '2.9',
         author = 'Lob',
         author_email = 'support@lob.com',
         packages = ['lob'],
