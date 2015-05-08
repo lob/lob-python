d48f1286f207c9decf7ff0bdf93c025878385855
Peter Nagel <pnagel5@gmail.com>
2015-05-08 09:15:58 -0700

    added HTML, csv example

---
+++ examples/csv_postcards/create_postcards.py
@@ -0,0 +1,50 @@
+import lob
+import csv
+import sys
+
+lob.api_key = 'test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc'
+
+skipFirstLine  = True
+
+try:
+  sys.argv[1]
+except IndexError:
+  print "Please provide an input CSV file as an argument."
+  sys.exit()
+
+with open('postcard_front.html', 'r') as frontHtmlFile:
+    frontHtml = frontHtmlFile.read()
+    with open('postcard_back.html', 'r') as backHtmlFile:
+        backHtml = backHtmlFile.read()
+        with open(sys.argv[1]) as f:
+            for row in csv.reader(f):
+                postcard = lob.Postcard.create(
+                    to_address = {
+                        'name': row[5],
+                        'address_line1': row[6],
+                        'address_line2': row[7],
+                        'address_city': row[8],
+                        'address_state': row[9],
+                        'address_zip': row[10],
+                        'address_country': row[11]
+                    },
+                    from_address = {
+                        'name': 'Lob',
+                        'address_line1': '123 Main Street',
+                        'address_city': 'San Francisco',
+                        'address_state': 'CA',
+                        'address_zip': '94185',
+                        'address_country': 'US'
+                    },
+                    setting = 1002,
+                    front = frontHtml,
+                    back = backHtml,
+                    data = {
+                        'background_image': row[1],
+                        'background_color': row[2],
+                        'name': row[0],
+                        'car': row[3],
+                        'mileage': row[4]
+                    }
+                )
+                print postcard.url
