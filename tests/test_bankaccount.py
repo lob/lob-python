ff45e61862d7d785dfe691ee0f94f24cb77f543d
Elnaz Moshfeghian <elnaz.mosh@gmail.com>
2016-02-01 07:22:16 +0000

    feat(version): support version 2016-01-19

--- tests/test_bankaccount.py
+++ tests/test_bankaccount.py
@@ -4,7 +4,7 @@ import lob
 class BankAccountFunctions(unittest.TestCase):
     def setUp(self):
         lob.api_key = 'test_fc26575412e92e22a926bc96c857f375f8b'
-        self.addr = lob.Address.list(count=1).data[0]
+        self.addr = lob.Address.list(limit=1).data[0]
 
     def test_list_bankAccounts(self):
         bankAccounts = lob.BankAccount.list()
@@ -12,12 +12,12 @@ class BankAccountFunctions(unittest.TestCase):
         self.assertEqual(bankAccounts.object, 'list')
 
     def test_list_bankAccounts_limit(self):
-        bankAccounts = lob.BankAccount.list(count=2)
+        bankAccounts = lob.BankAccount.list(limit=2)
         self.assertTrue(isinstance(bankAccounts.data[0], lob.BankAccount))
         self.assertEqual(len(bankAccounts.data), 2)
 
     def test_list_bankAccounts_fail(self):
-        self.assertRaises(lob.error.InvalidRequestError, lob.BankAccount.list, count=1000)
+        self.assertRaises(lob.error.InvalidRequestError, lob.BankAccount.list, limit=1000)
 
     def test_create_bankAccount_fail(self):
         self.assertRaises(lob.error.InvalidRequestError, lob.BankAccount.create)
--- tests/test_setting.py
+++ tests/test_setting.py
@@ -9,12 +9,9 @@ class SettingFunctions(unittest.TestCase):
         settings = lob.Setting.list()
         self.assertEqual(settings.object, 'list')
 
-
     def test_find_setting(self):
         setting = lob.Setting.retrieve(id=200)
         self.assertEqual(setting.id, '200')
 
     def test_find_setting_fail(self):
         self.assertRaises(lob.error.InvalidRequestError, lob.Setting.retrieve, id='test')
-
-
