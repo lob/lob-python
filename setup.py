bdd190b84827628c6c4dc4bb3df8fa75cbb6647c
Stewart Lynch <stewart.j.lynch@gmail.com>
2016-09-06 14:17:25 -0700

    fix(readme): Remove 3.2 from the readme and add 3.5

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup (
             "Development Status :: 5 - Production/Stable",
             "License :: OSI Approved :: MIT License",
             "Programming Language :: Python",
+            "Programming Language :: Python :: 2.6",
             "Programming Language :: Python :: 2.7",
             "Programming Language :: Python :: 3",
             "Programming Language :: Python :: 3.3",
