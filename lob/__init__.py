ce398a78b4a140157a02e303583c90d723b93dcd
Donald Little <donald@lob.com>
2016-09-30 15:05:06 -0700

    chore(jobs): remove deprecated endpoints

--- lob/__init__.py
+++ lob/__init__.py
@@ -2,7 +2,7 @@ api_key = None
 api_base = 'https://api.lob.com/v1'
 
 #Resources
-from lob.resource import (Address, Area, BankAccount, Check, Country, Job,
-    Letter, Object, Postcard, Route, Setting, State, Verification)
+from lob.resource import (Address, Area, BankAccount, Check, Country,
+        Letter, Postcard, Route, State, Verification)
 
 from lob.version import VERSION
