29f8f0ec4147f7010b8efc674a20e8b893a8ea7a
Donald Little <donald@lob.com>
2017-05-22 19:55:57 -0700

    feat(examples): add US Verification API example

--- examples/verify_addresses_from_csv/verify.py
+++ examples/verify_addresses_from_csv/verify.py
@@ -21,14 +21,13 @@ if len(sys.argv) < 2:
 
 input_filename = sys.argv[1]
 
-success_csv_fields = [
-    'address_line1',
-    'address_line2',
-    'address_city',
-    'address_state',
-    'address_zip'
+verifications_csv_fields = [
+    'primary_line',
+    'secondary_line',
+    'urbanization',
+    'last_line',
+    'deliverability'
 ]
-errors_csv_fields = ['error']
 
 #create the output directory,
 output_dir = os.path.join('.',  'output')
@@ -46,55 +45,37 @@ except Exception as e:
     sys.exit(1)
 
 # output csv names
-success_filename = os.path.join(output_dir, 'success.csv')
-errors_filename = os.path.join(output_dir, 'errors.csv')
+verifications_filename = os.path.join(output_dir, 'verifications.csv')
 
 try:
     with open(input_filename, 'r') as input, \
-         open(success_filename, 'w') as success, \
-         open(errors_filename, 'w') as errors:
-
-    # print mode to screen
+        open(verifications_filename, 'w') as verifications:
 
         input_csv = csv.DictReader(input)
-        errors_csv_fields += input_csv.fieldnames
 
-        success_csv = csv.DictWriter(success, fieldnames=success_csv_fields)
-        errors_csv = csv.DictWriter(errors, fieldnames=errors_csv_fields)
+        verifications_csv = csv.DictWriter(verifications, fieldnames=verifications_csv_fields)
+        verifications_csv.writeheader()
 
-        err_count = 0
-
-        # Loop through input CSV rows
         for idx, row in enumerate(input_csv):
-            # Create letter from row
-            try:
-                verifiedAddress = lob.Verification.create(
-                    address_line1=row['address1'],
-                    address_line2=row['address2'],
-                    address_city=row['city'],
-                    address_state=row['state'],
-                    address_zip=row['postcode'],
-                    address_country='US'
-                )
-            except Exception as e:
-                error_row = {'error': e}
-                error_row.update(row)
-                errors_csv.writerow(error_row)
-                err_count += 1
-                sys.stdout.write('E')
-                sys.stdout.flush()
-            else:
-                success_csv.writerow({
-                    'address_line1': verifiedAddress.address.address_line1,
-                    'address_line2': verifiedAddress.address.address_line2,
-                    'address_city':  verifiedAddress.address.address_city,
-                    'address_state': verifiedAddress.address.address_state,
-                    'address_zip':   verifiedAddress.address.address_zip
-                })
+            verifiedAddress = lob.USVerification.create(
+                primary_line=row['primary_line'],
+                secondary_line=row['secondary_line'],
+                city=row['city'],
+                state=row['state'],
+                zip_code=row['zip_code'],
+            )
 
-                # Print success
-                sys.stdout.write('.')
-                sys.stdout.flush()
+            verifications_csv.writerow({
+                'primary_line': verifiedAddress.primary_line,
+                'secondary_line': verifiedAddress.secondary_line,
+                'urbanization':  verifiedAddress.urbanization,
+                'last_line':  verifiedAddress.last_line,
+                'deliverability': verifiedAddress.deliverability
+            })
+
+            # Print success
+            sys.stdout.write('.')
+            sys.stdout.flush()
 
             # New lines for larger csv's
             if idx % 10 is 9:
@@ -106,5 +87,4 @@ except Exception as e:
     sys.exit(1)
 
 print('')
-print('Done with ' + (str(err_count) if err_count else 'no') + ' errors.')
 print('Results written to ' + str(output_dir) + '.')
