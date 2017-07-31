ee271ff333505015a6be787a5fa84bcdf8c94447
Scott Hasbrouck <scott@lob.com>
2017-07-31 12:03:47 -0700

    feat(us-zip-lookups): Add US Zip Code Lookup

--- lob/__init__.py
+++ lob/__init__.py
@@ -3,6 +3,6 @@ api_base = 'https://api.lob.com/v1'
 
 #Resources
 from lob.resource import (Address, Area, BankAccount, Check, Country,
-        Letter, Postcard, Route, State, USVerification, IntlVerification)
+        Letter, Postcard, Route, State, USVerification, USZipLookup, IntlVerification)
 
 from lob.version import VERSION
