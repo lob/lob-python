a0d3b0e6e7c28c5dc8412a9e38422febdafe0155
Donald Little <dmlittle94@gmail.com>
2018-03-13 11:48:21 -0700

    feat(CreateableResource): allow query params in creatable resources (#145)

--- tests/test_us_verification.py
+++ tests/test_us_verification.py
@@ -8,11 +8,25 @@ class TestUSVerificationFunctions(unittest.TestCase):
 
     def test_us_verification(self):
         addr = lob.USVerification.create(
-            primary_line='185 Berry St Ste 6600',
+            primary_line='deliverable',
             city='San Francisco',
             state='CA',
             zip_code='94107'
         )
 
         self.assertEqual(addr.deliverability, 'deliverable')
-        self.assertEqual(addr.primary_line, '185 BERRY ST STE 6600')
+        self.assertEqual(addr.primary_line, '1 TELEGRAPH HILL BLVD')
+
+    def test_us_verification_format(self):
+        addr = lob.USVerification.create(
+            primary_line='deliverable',
+            city='San Francisco',
+            state='CA',
+            zip_code='94107',
+            query={
+                'case': 'proper'
+            }
+        )
+
+        self.assertEqual(addr.deliverability, 'deliverable')
+        self.assertEqual(addr.primary_line, '1 Telegraph Hill Blvd')
