658d6790cfbc1fa20a6740671a143dfddcbdbfc6
Donald Little <donald@lob.com>
2018-05-01 15:05:24 -0400

    3.3.0

--- lob/version.py
+++ lob/version.py
@@ -1,3 +1,3 @@
 from __future__ import unicode_literals
 
-VERSION = '3.2.0'
+VERSION = '3.3.0'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 setup(
     name='lob',
-    version='3.2.0',
+    version='3.3.0',
     author='Lob',
     author_email='support@lob.com',
     packages=['lob'],
