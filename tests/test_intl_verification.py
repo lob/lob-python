b6adbdd62aafeab8789adec527a6de9bb17add33
Aditi <aditi@lob.com>
2019-12-16 17:26:40 -0800

    feat(intl-verification): updating python github library

--- tests/test_intl_verification.py
+++ tests/test_intl_verification.py
@@ -8,13 +8,10 @@ class TestIntlVerificationFunctions(unittest.TestCase):
         lob.api_key = os.environ.get('LOB_API_KEY')
 
     def test_intl_verification(self):
-        try:
-            lob.IntlVerification.create(
-                primary_line='370 Water St',
-                city='Summerside',
-                state='Prince Edward Island',
-                postal_code='C1N 1C4',
-                country='CA'
-            )
-        except Exception as e:
-            self.assertEqual(e.http_status, 403)
+        lob.IntlVerification.create(
+            primary_line='deliverable',
+            country='CA'
+        )
+
+        self.assertEqual(addr.deliverability, 'deliverable')
+        self.assertEqual(addr.primary_line, '370 WATER ST')
