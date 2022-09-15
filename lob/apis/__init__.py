2485b43ed10b513da23525ffab1a9671a1dd4768
sudoku-lord <aditi@lob.com>
2022-09-15 15:58:31 -0700

    generated SDK branch

---
+++ lob/apis/__init__.py
@@ -0,0 +1,38 @@
+
+# flake8: noqa
+
+# Import all APIs into this package.
+# If you have many APIs here with many many models used in each API this may
+# raise a `RecursionError`.
+# In order to avoid this, import only the API that you directly need like:
+#
+#   from .api.addresses_api import AddressesApi
+#
+# or import this package, but before doing it, use:
+#
+#   import sys
+#   sys.setrecursionlimit(n)
+
+# Import APIs into API package:
+from lob_python.api.addresses_api import AddressesApi
+from lob_python.api.bank_accounts_api import BankAccountsApi
+from lob_python.api.billing_groups_api import BillingGroupsApi
+from lob_python.api.campaigns_api import CampaignsApi
+from lob_python.api.card_orders_api import CardOrdersApi
+from lob_python.api.cards_api import CardsApi
+from lob_python.api.checks_api import ChecksApi
+from lob_python.api.creatives_api import CreativesApi
+from lob_python.api.identity_validation_api import IdentityValidationApi
+from lob_python.api.intl_autocompletions_api import IntlAutocompletionsApi
+from lob_python.api.intl_verifications_api import IntlVerificationsApi
+from lob_python.api.letters_api import LettersApi
+from lob_python.api.postcards_api import PostcardsApi
+from lob_python.api.reverse_geocode_lookups_api import ReverseGeocodeLookupsApi
+from lob_python.api.self_mailers_api import SelfMailersApi
+from lob_python.api.template_versions_api import TemplateVersionsApi
+from lob_python.api.templates_api import TemplatesApi
+from lob_python.api.uploads_api import UploadsApi
+from lob_python.api.us_autocompletions_api import UsAutocompletionsApi
+from lob_python.api.us_verifications_api import UsVerificationsApi
+from lob_python.api.zip_lookups_api import ZipLookupsApi
+from lob_python.api.default_api import DefaultApi
--- lob/compat.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-
-if sys.version_info[0] == 3:  # pragma: no cover
-    string_type = str
-    from io import BytesIO
-else:  # pragma: no cover
-    string_type = basestring
-    from StringIO import StringIO as BytesIO
