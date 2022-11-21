eabac4133b8e7c2c17a1c4d1b83d61a477219141
BennyKitchell <bennykitchell@gmail.com>
2022-11-21 15:37:01 -0600

    chore: bump version for release

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "lob-python"
-VERSION = "5.0.0"
+VERSION = "5.0.1"
 # To install the library, run the following
 #
 # python setup.py install
